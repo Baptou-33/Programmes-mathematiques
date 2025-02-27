2c3384d5ac847bbd145ce45dac45d7c7bb599701
Baptiste <baptiste.dureau@outlook.fr>
2025-02-27 19:14:03 +0100

    Ajout bibliothèque decimal

--- Pi.py
+++ Pi.py
@@ -1,66 +1,57 @@
 from random import *
 from math import *
+import decimal
+from decimal import Decimal as D
 
 p = int(input("Précision : "))
+decimal.getcontext().prec = p
 
 def Monte_Carlo(n):
     bons_points = 0
     for i in range(n):
-        x =uniform(0, 1)
-        y =uniform(0, 1)
-        if x**2 + y**2 <= 1:
+        x = uniform(0, 1)
+        y = uniform(0, 1)
+        if D(x)*D(x) + D(y)*D(y) <= D(1):
             bons_points += 1
-    return 4*bons_points/n
+    return D(4)*D(bons_points)/D(n)
 
 def Madhava_Leibniz(n):
-    pi = 0
+    pi = D(0)
     for i in range(n):
-        pi += (-1)**i/(1+2*i)
-    return 4*pi
-
-def John_Machin(n):
-    pi = 0
-    for i in range(n):
-        pi += (-1)**i/(1+2*i)*(4/5**(1+2*i)-1/239**(1+2*i))
-    return 4*pi
+        pi += D(-1)**D(i)/(D(1)+D(2)*D(i))
+    return D(4)*pi
 
 def Riemann(n):
-    pi = 0
+    pi = D(0)
     for i in range(1, n):
-        pi += 1/i**2
-    return sqrt(6*pi)
-
-def Gauss_Legendre(n):
-    a = 1
-    b = 1/sqrt(2)
-    p = 1
-    t = 0.25
-    for i in range(1, n):
-        a_new = (a+b)/2
-        b_new = sqrt(a*b)
-        p_new = 2*p
-        t = t-p*(a_new-a)**2
-        a = a_new
-        b = b_new
-        p = p_new
-
-    return (a+b)**2/(4*t)
+        pi += D(1)/D(i)**D(2)
+    return D(D(6)*pi).sqrt()
 
 def Wallis(n):
-    pi = 1
+    pi = D(1)
     for i in range(1,n):
-        pi *= 4*i**2/((2*i-1)*(2*i+1))
-    return 2*pi
+        pi *= D(4)*D(i)**D(2)/((D(2)*D(i)-D(1))*(D(2)*D(i)+D(1)))
+    return D(2)*pi
 
-if p <= 1024:
-    print(Gauss_Legendre(p))
-else:
-    print(Gauss_Legendre(1024))
-print(Monte_Carlo(p))
-print(Madhava_Leibniz(p))
-print(Riemann(p))
-print(Wallis(p))
-if p < 11:
-    print(John_Machin(p))
-else:
-    print(John_Machin(11))
+def John_Machin(n):
+    pi = D(0)
+    for i in range(n):
+        pi += D(-1)**D(i)/(D(1)+D(2)*D(i))*(D(4)/D(5)**(D(1)+D(2)*D(i))-D(1)/D(239)**(D(1)+D(2)*D(i)))
+    return D(4)*pi
+
+def Gauss_Legendre(n):
+    a = D(1)
+    b = D(1)/D(2).sqrt()
+    p = D(1)
+    t = D(0.25)
+    for i in range(1, n):
+        a, b, p, t = (a+b)/D(2), D(a*b).sqrt(), D(2)*p, t-p*((a+b)/D(2)-a)**D(2)
+    return (a+b)**D(2)/(D(4)*t)
+
+
+print("Monte-Carlo :    ", Monte_Carlo(p))
+print("Madhava-Leibniz :", Madhava_Leibniz(p))
+print("Riemann :        ", Riemann(p))
+print("Wallis :         ", Wallis(p))
+print("John Machin :    ", John_Machin(p))
+print("Gauss Legendre : ", Gauss_Legendre(p))
