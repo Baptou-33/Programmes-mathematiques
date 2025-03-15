2a00aed0e4189585da21baaedffbb00cd48f2041
Baptiste <baptiste.dureau@outlook.fr>
2025-03-15 19:30:14 +0100

    Peur maintenant calculer une racine n-ième avec 27 décimales au maximum

--- sqrt.py
+++ sqrt.py
@@ -2,31 +2,33 @@ from math import *
 from decimal import *
 from decimal import Decimal as D
 
-def sqrt(n, p):
+def sqrt(n, g, p):
     k = 10**D(n).log10().__floor__()
+    g = D(g)
     sqrt = D(0)
-    while not sqrt**D(2) == n and k > 10**(-D(p)-D(2)):
-        print(round(100*D(k).log10()/D(10**(-D(p)-D(2))).log10()),"%")
-        if sqrt**D(2) < n:
-            while sqrt**D(2) <= n:
+    while not sqrt**g == n and k > 10**(-D(p)-D(2)):
+        if sqrt**g < n:
+            while sqrt**g <= n:
                 sqrt += k
                 if sqrt + k == sqrt:
-                    print("Maximum atteint")
+                    print("\nMaximum atteint (27 décimales)")
                     return sqrt
             sqrt -= k
         else:
-            while sqrt**D(2)>= n and sqrt >= D(0):
+            while sqrt**g >= n and sqrt >= D(0):
                 print("-")
                 sqrt -= k
             sqrt += k
         k /= D(10)
-    if sqrt**D(2) == n:
+    if sqrt**g == n:
         return sqrt
     else:
         return round(sqrt, p)
 
-a = int(input("Racine carrée de :"))
+a = int(input("Racine de :"))
 assert a >= 0
-b = int(input("Nombre de décimales :"))
-assert b >= 1
-print(sqrt(a, b))
+b = int(input("Racine combien-ième ? "))
+assert b >=1
+c = int(input("Nombre de décimales :"))
+assert c >= 1
+print("\nsqrt(", a, ",", b, ") = ", sqrt(a, b, c))
