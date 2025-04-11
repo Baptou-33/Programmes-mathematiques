705d4ff36cd948e56e69f316c8c77f8e8d931499
Baptiste <baptiste.dureau@outlook.fr>
2025-04-11 18:07:06 +0200

    amélioration du calculateur de nombres premiers

--- Nombres premiers.py
+++ Nombres premiers.py
@@ -1,19 +1,21 @@
-n=int(input("Nombre premier n°"))
-if n<0 or round(n)!=n:
-    print("Nombre invalide")
-elif n==1:
-    print("Le 1 er nombre premier est 2")
-else:
-    nombres = [2]
-    vérifiés = 3
-    while len(nombres) < n:
-        i = 0
-        while i < len(nombres) and vérifiés % nombres[i] != 0:
-            i += 1
-        if i == len(nombres):
-            nombres.append(vérifiés)
-            if len(nombres)%1000==0 and n>1000:
-                print(100*len(nombres)/n, "%")
-        vérifiés += 2
+from math import sqrt
 
-    print("Le", n, "ième nombre premier est", nombres[-1])
+p=[2,3]
+
+def premier(n):
+    if n<1 or round(n)!=n:
+        return "Nombre invalide"
+
+    k = p[-1]
+    while len(p) < n:
+        k += 2
+        i = 0
+        while p[i] <= sqrt(k) and k % p[i] != 0:
+            i += 1
+        if p[i] > sqrt(k):
+            p.append(k)
+
+    return p[n-1]
+
+while True:
+    print(premier(int(input("Nombre premier n°"))))
